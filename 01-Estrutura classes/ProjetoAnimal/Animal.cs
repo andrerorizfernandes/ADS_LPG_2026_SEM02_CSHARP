0e968e98fc6650593bc54fd4182a31ea7c1c73e9
andrerorizfernandes <andremuriae@yahoo.com.br>
2026-08-13 21:47:09 -0300

    Arquivos do projeto de classes 03

---
+++ 01-Estrutura classes/ProjetoAnimal/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAnimal
+{
+    public class Animal
+    {
+        public string Raca { get; set; }
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public string Genero { get; set; }
+        public int NumeroPatas { get; set; }
+        public string CorOlhos { get; set; }
+
+        public Animal()
+        {
+            NumeroPatas = 0;
+        }
+
+        public Animal(int numeroPatas)
+        {
+            NumeroPatas = numeroPatas;
+        }
+
+        public string EmitirSom()
+        {
+            return "O animal emite um som.";
+        }
+
+        public string Correr(bool emMovimento = true)
+        {
+            if (emMovimento)
+                return "O animal está correndo.";
+            else
+                return "O animal não está correndo.";
+        }
+
+        public string Reproduzir()
+        {
+            return "O animal está se reproduzindo.";
+        }
+    }
+}
---
+++ 01-Estrutura classes/ProjetoAnimal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAnimal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var quantidadePatas = 4;
+            Animal animal = new Animal(quantidadePatas);
+
+            Console.WriteLine("Informe a raça do animal:");
+            animal.Raca = Console.ReadLine();
+
+            Console.WriteLine("Informe o nome do animal:");
+            animal.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe a cor do animal:");
+            animal.Cor = Console.ReadLine();
+
+            Console.WriteLine("Informe o gênero do animal:");
+            animal.Genero = Console.ReadLine();
+
+            Console.WriteLine("Informe a cor dos olhos do animal:");
+            animal.CorOlhos = Console.ReadLine();
+
+            Console.WriteLine(
+                animal.Raca + "\n" + 
+                animal.Nome + "\n" + 
+                animal.Cor + "\n" + 
+                animal.Genero + "\n" + 
+                animal.NumeroPatas + "\n" + 
+                animal.CorOlhos);
+
+            var resultado = animal.Correr(false);
+            Console.WriteLine(resultado);
+        }
+    }
+}
