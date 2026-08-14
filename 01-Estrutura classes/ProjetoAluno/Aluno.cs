d52958192a8fd5b01b0d55544bbee3b1e646db01
andrerorizfernandes <andremuriae@yahoo.com.br>
2026-08-13 21:18:44 -0300

    Projeto de construção de classes 02

---
+++ 01-Estrutura classes/ProjetoAluno/Aluno.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAluno
+{
+    public class Aluno
+    {
+        private int matricula;
+        private string nome;
+        private string sexo;
+        private string telefone;
+        private string endereco;
+        private string nomePai;
+        private string nomeMae;
+        private string escolaridade;
+
+        public int GetMatricula()
+        {
+            return matricula;
+        }
+
+        public void SetMatricula(int matricula)
+        {
+            this.matricula = matricula;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string GetSexo()
+        {
+            return sexo;
+        }
+
+        public void SetSexo(string sexo)
+        {
+            this.sexo = sexo;
+        }
+
+        public string GetTelefone()
+        {
+            return telefone;
+        }
+
+        public void SetTelefone(string telefone)
+        {
+            this.telefone = telefone;
+        }
+
+        public string GetEndereco()
+        {
+            return endereco;
+        }
+
+        public void SetEndereco(string endereco)
+        {
+            this.endereco = endereco;
+        }
+
+        public string GetNomePai()
+        {
+            return nomePai;
+        }
+
+        public void SetNomePai(string nomePai)
+        {
+            this.nomePai = nomePai;
+        }
+
+        public string GetNomeMae()
+        {
+            return nomeMae;
+        }
+
+        public void SetNomeMae(string nomeMae)
+        {
+            this.nomeMae = nomeMae;
+        }
+
+        public string Getescolaridade()
+        {
+            return escolaridade;
+        }
+
+        public void SetEscolaridade(string escolaridade)
+        {
+            this.escolaridade = escolaridade;
+        }
+
+        public string Matricular(int matricula)
+        {
+            this.matricula = matricula;
+            return "Aluno " + matricula + " matriculado com sucesso!";
+        }
+
+        public string Transferir(int matricula)
+        {
+            this.matricula = matricula;
+            return "Aluno " + matricula + " transferido com sucesso!";
+        }
+
+        public string Evadir(int matricula)
+        {
+                this.matricula = matricula;
+                return "Aluno " + matricula + " evadido com sucesso!";
+        }
+    }
+}
---
+++ 01-Estrutura classes/ProjetoAluno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAluno
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno alTeste = new Aluno();
+            alTeste.SetMatricula(123);
+            alTeste.SetNome("João da Silva");  
+            alTeste.SetSexo("Masculino");
+            alTeste.SetTelefone("(11) 98765-4321");
+            alTeste.SetEndereco("Rua das Flores, 123");
+            alTeste.SetNomePai("José da Silva");
+            alTeste.SetNomeMae("Maria da Silva");
+            alTeste.SetEscolaridade("Ensino Médio Completo");
+
+            alTeste.Matricular(689);
+
+            Console.WriteLine(alTeste.GetMatricula());
+            Console.WriteLine(alTeste.GetNome());
+            Console.WriteLine(alTeste.GetSexo());
+            Console.WriteLine(alTeste.GetTelefone());
+            Console.WriteLine(alTeste.GetEndereco());
+            Console.WriteLine(alTeste.GetNomePai());
+            Console.WriteLine(alTeste.GetNomeMae());
+            Console.WriteLine(alTeste.Getescolaridade());        
+        }
+    }
+}
