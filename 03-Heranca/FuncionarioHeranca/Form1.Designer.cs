f794fe2559167d9d99c40473b8b18b8afcb47d37
andrerorizfernandes <andremuriae@yahoo.com.br>
2026-09-03 21:36:14 -0300

    Implementações da aula do dia 03/09/2026

---
+++ 03-Heranca/FuncionarioHeranca/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FuncionarioHeranca
+{
+    partial class frmFuncionario
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTipoFuncionario = new System.Windows.Forms.ComboBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboTipoFuncionario
+            // 
+            this.cboTipoFuncionario.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTipoFuncionario.FormattingEnabled = true;
+            this.cboTipoFuncionario.Items.AddRange(new object[] {
+            "Funcionário",
+            "Gerente"});
+            this.cboTipoFuncionario.Location = new System.Drawing.Point(12, 35);
+            this.cboTipoFuncionario.Name = "cboTipoFuncionario";
+            this.cboTipoFuncionario.Size = new System.Drawing.Size(167, 21);
+            this.cboTipoFuncionario.TabIndex = 0;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(12, 79);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(167, 20);
+            this.txtSalario.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 124);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 219);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.cboTipoFuncionario);
+            this.Name = "frmFuncionario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pagamento de salário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboTipoFuncionario;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ 03-Heranca/FuncionarioHeranca/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionarioHeranca
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public int Idade { get; set; }
+        public double Salario { get; private set; }
+
+        public virtual double PagarSalario(double salario)
+        {
+            return salario;
+        }
+    }
+}
