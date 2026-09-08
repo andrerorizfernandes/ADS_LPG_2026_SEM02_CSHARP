25a8a4bad22505510d576dd5fe222a7c73e2655f
andrerorizfernandes <andremuriae@yahoo.com.br>
2026-09-08 19:32:36 -0300

    Exemplos de polimorfismo

---
+++ 04-Polimorfismo/ExemploPolimorfismo/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPolimorfismo
+{
+    public class Aluno
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public Aluno(int idade, string cpf)
+        {
+            this.Cpf = cpf;
+        }
+
+        public Aluno(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        public Aluno() { 
+        
+        }
+
+        public void CadastrarAluno()
+        {
+
+        }
+
+        public void CadastrarAluno(bool Ativo)
+        {
+
+        }
+
+        public void CadastrarAluno(bool Ativo, string nome)
+        {
+
+        }
+    }
+}
---
+++ 04-Polimorfismo/ExemploPolimorfismo/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploPolimorfismo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aluno xpto = new Aluno();
+            xpto.CadastrarAluno();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conta c1 = new Conta();
+            ContaPoupanca c2 = new ContaPoupanca();
+            TotalizadorContas t = new TotalizadorContas();
+
+            t.Soma(c1);
+            t.Soma(c2);
+        }
+    }
+}
